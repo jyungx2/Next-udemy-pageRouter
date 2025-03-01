771f44a51d2b76473ff7fcebe66c73f3d7aacb74
jiyoungnim <cloundyon31@gmail.com>
2025-03-01 20:38:44 +0900

    🎉 initial commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// index.js 파일을 생성하면, Next.js는 이 파일을 **루트 페이지(/)**로 인식하여 기본적으로 렌더링합니다.
+function HomePage() {
+  return (
+    <div>
+      <h1>The Home Page</h1>
+    </div>
+  );
+}
+
+export default HomePage;
