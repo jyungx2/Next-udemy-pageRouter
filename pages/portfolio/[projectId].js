94a1c0a837601b5faa56065cc26abe84c10b047f
jiyoungnim <cloundyon31@gmail.com>
2025-03-01 21:25:49 +0900

    📝 236. Extracting Dynamic Path Segment Data (Dynamic Routes)

--- pages/portfolio/[projectId].js
+++ pages/portfolio/[projectId].js
@@ -1,4 +1,11 @@
+import { useRouter } from "next/router";
+
 function PortfolioProjectPage() {
+  console.log(useRouter.pathname); // /portfolio/[projectId]
+  console.log(useRouter.query); // {} -> {projectId: "something"} => gives us access to the concrete data that is encoded in the URL.
+
+  // send a request to some backend server to fetch the piece of data with an id of router.query.projectId
+
   return (
     <div>
       <h1>The Portfolio Project Page</h1>
