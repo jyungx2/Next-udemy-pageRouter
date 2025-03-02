f752926d9f2907888f3a7c5ce9e87ea329b42f18
jiyoungnim <cloundyon31@gmail.com>
2025-03-02 21:10:36 +0900

    📝 240. Navigating To Dynamic Routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,19 @@
 function ClientPage() {
+  const clients = [
+    { id: "max", name: "Maximillian" },
+    { id: "manu", name: "Manuel" },
+  ];
+
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => {
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>;
+        })}
+      </ul>
     </div>
   );
 }
