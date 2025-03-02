e2eb2ae6672d721e4f183fb229c1dd0387bfac18
jiyoungnim <cloundyon31@gmail.com>
2025-03-02 21:25:49 +0900

    📝 241. A Different Way Of Setting Link Hrefs

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 function ClientPage() {
   const clients = [
     { id: "max", name: "Maximillian" },
@@ -8,11 +10,20 @@ function ClientPage() {
     <div>
       <h1>The Clients Page</h1>
       <ul>
-        {clients.map((client) => {
+        {clients.map((client) => (
+          // 💥 Link 컴포넌트의 href 속성에 문자열을 전달하는 방식이 일반적, 하지만 동적 경로를 직접 문자열로 조합하는 것이 번거로울 수 있음.
+          // ✅ 대안: 객체(Object) 기반 href
+          // Next.js에서는 href에 객체를 전달하는 방법도 지원함.
+          // pathname과 query 속성을 이용해 동적 경로를 정의 가능.
+          // 객체 방식을 사용하면 동적 라우트를 보다 명확하게 정의 가능.
           <li key={client.id}>
-            <Link href={`/clients/${client.id}`}>{client.name}</Link>
-          </li>;
-        })}
+            <Link
+              href={{ pathname: "/clients/[id]", query: { id: client.id } }}
+            >
+              {client.name}
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
